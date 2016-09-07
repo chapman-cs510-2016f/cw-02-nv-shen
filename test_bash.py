2109937c3c119c7db5cbf4884879b53b1d9445d2
helen77 <zny_ok1989@hotmail.com>
2016-09-07 04:43:15 +0000

    Changed test_bash.py

--- test_bash.py
+++ test_bash.py
@@ -4,4 +4,10 @@ def test_helloworld():
 	OUTPUT=subprocess.check_output("./helloworld.sh")
 	assert 'Hello World.\n'== OUTPUT, 'The output of helloworld.sh is: '+OUTPUT+',but not "Hello World.\\n"'	
 
+def test_countup():
+	OUTPUT=subprocess.check_output(["./countup.sh","5"]) 
+	assert '1 2 3 4 5\n' == OUTPUT, 'The output of countup.sh is: '+OUTPUT+',but not "1 2 3 4 5\\n"'
+
+
 test_helloworld()
+test_countup()
