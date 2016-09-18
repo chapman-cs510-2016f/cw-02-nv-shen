ef08f3214fc8a4b45980c8fb1111b5c868d7bfe5
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-17 23:52:10 -0700

    instructor comments

--- helloworld.sh
+++ helloworld.sh
@@ -3,3 +3,9 @@
 echo  "Hello World."
 
 exit 0
+
+### INSTRUCTOR COMMENTS
+# Note: you don't have to put exit 0 at the end, since it's the default behavior
+# Following UNIX philosophy, only do things when they mean something, and try
+# not to repeat work that has already been done. Defaults should be used when
+# possible. (This is also known as the "principle of least surprise".)
