c122135c2f2eb95db1a5bad969e5baee83510d70
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-18 00:07:42 -0700

    instructor comments

--- test_bash.py
+++ test_bash.py
@@ -11,3 +11,14 @@ def test_countup():
 
 test_helloworld()
 test_countup()
+
+### INSTRUCTOR COMMENTS
+# Remember to include docstrings for both your module and your functions. 
+# This is a good habit to get into, and will help users of your code use
+# the python help system to figure out what your code does and why it is there.
+#
+# Do not execute code at the end of your test script (the last two lines).
+# Instead, the command "nosetests" when run from a bash shell will look for
+# files starting with "test" and run all functions starting with "test" 
+# automatically. Also, if you need to run executable code in a script in the
+# future, be sure to put it inside an if __name__ == "__main__" block.
